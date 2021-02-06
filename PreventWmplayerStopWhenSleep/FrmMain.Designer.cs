237c617bd532139cf466ef6916af50af942a3b60
Joma <megamanx@outlook.com>
2021-02-06 17:40:11 -0500

    2nd

--- PreventWmplayerStopWhenSleep/FrmMain.Designer.cs
+++ PreventWmplayerStopWhenSleep/FrmMain.Designer.cs
@@ -29,47 +29,18 @@ namespace PreventSleep
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
-            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelElapsed = new System.Windows.Forms.Label();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
-            // timerRefresh
-            // 
-            this.timerRefresh.Enabled = true;
-            this.timerRefresh.Interval = 1000;
-            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ElapsedTime";
-            // 
-            // labelElapsed
-            // 
-            this.labelElapsed.AutoSize = true;
-            this.labelElapsed.Location = new System.Drawing.Point(100, 9);
-            this.labelElapsed.Name = "labelElapsed";
-            this.labelElapsed.Size = new System.Drawing.Size(16, 13);
-            this.labelElapsed.TabIndex = 1;
-            this.labelElapsed.Text = "...";
-            // 
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 34);
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(256, 189);
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(256, 211);
             this.axWindowsMediaPlayer1.TabIndex = 2;
             // 
             // FrmMain
@@ -78,23 +49,16 @@ namespace PreventSleep
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(286, 239);
             this.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Controls.Add(this.labelElapsed);
-            this.Controls.Add(this.label1);
             this.Name = "FrmMain";
             this.Text = "Prevent Stop when Sleep - Wmplayer";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer timerRefresh;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelElapsed;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
--- PreventWmplayerStopWhenSleep/FrmMain.cs
+++ PreventWmplayerStopWhenSleep/FrmMain.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PreventSleep
 {
     public partial class FrmMain : Form
     {
-        const int REFRESH_MILLISECONDS = 10000;
+        const int REFRESH_MILLISECONDS = 30000;//Chance this value. 30000ms = 30s.
 
         [Flags]
         public enum ExecutionState : uint 
@@ -33,6 +26,8 @@ namespace PreventSleep
 
 
         Stopwatch stopwatch;
+        private Timer timerRefresh;
+
         public FrmMain()
         {
             InitializeComponent();
@@ -46,21 +41,26 @@ namespace PreventSleep
             axWindowsMediaPlayer1.settings.setMode("loop", true);
             axWindowsMediaPlayer1.Ctlcontrols.play();
             axWindowsMediaPlayer1.uiMode = "none";
+
+            timerRefresh = new Timer();
+            timerRefresh.Enabled = true;
+            timerRefresh.Interval = 1000;
+            timerRefresh.Tick += new EventHandler(this.timerRefresh_Tick);
         }
 
         private void timerRefresh_Tick(object sender, EventArgs e)
         {
-            labelElapsed.Text = stopwatch.ElapsedMilliseconds / 1000 + " seconds.";
             if((stopwatch.ElapsedMilliseconds / REFRESH_MILLISECONDS) == 1 )
             {
-                labelElapsed.Text = "SetThreadExecutionState is called.";
                 stopwatch.Restart();
-                SetThreadExecutionState(ExecutionState.ES_CONTINUOUS | ExecutionState.ES_AWAYMODE_REQUIRED);
+                SetThreadExecutionState(ExecutionState.ES_CONTINUOUS | ExecutionState.ES_AWAYMODE_REQUIRED ); //Display is OFF but the system is active.
+                //SetThreadExecutionState(ExecutionState.ES_CONTINUOUS | ExecutionState.ES_AWAYMODE_REQUIRED | ExecutionState.ES_DISPLAY_REQUIRED); //Display is allways ON.
             }
         }
 
         private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
         {
+            stopwatch.Stop();
             axWindowsMediaPlayer1.Ctlcontrols.stop();
             SetThreadExecutionState(ExecutionState.ES_CONTINUOUS);
         }
